dfd891f04c2a15964b2560f64fcd175e1d517579
GallowDove <gallowsdove@gallows.dev>
2025-09-23 01:13:49 +0200

    Update configs

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,13 +3,10 @@ local servers = require'lsp-servers'.get()
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup {
+  vim.lsp.config(lsp, {
 	capabilities = capabilities,
-    on_attach = require'lsp-keymaps',
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
+    on_attach = require'lsp-keymaps'
+  })
+  vim.lsp.enable(lsp)
 end
 
